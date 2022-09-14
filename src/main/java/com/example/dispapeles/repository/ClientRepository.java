bf520c5674ea71e5ddfafa773cb93b1b476c032c
GiovyUrbina <alejoub1998@gmail.com>
2022-09-13 20:21:15 -0500

    Configuracion base de datos PostgreSQLL

--- src/main/java/com/example/dispapeles/repository/ClientRepository.java
+++ src/main/java/com/example/dispapeles/repository/ClientRepository.java
@@ -1,7 +1,9 @@
 package com.example.dispapeles.repository;
 import com.example.dispapeles.model.Client;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface ClientRepository extends CrudRepository<Client, Integer>{
 
 }
