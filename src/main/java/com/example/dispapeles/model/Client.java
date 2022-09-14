c30a983022a31638f8f822e627cebf3401c63e38
GiovyUrbina <alejoub1998@gmail.com>
2022-09-13 19:13:15 -0500

    Creacion modelos

--- src/main/java/com/example/dispapeles/model/Client.java
+++ src/main/java/com/example/dispapeles/model/Client.java
@@ -1,2 +1,59 @@
-package com.example.dispapeles.model;public class Client {
+package com.example.dispapeles.model;
+
+public class Client {
+
+    private String typeId;
+    private String name;
+    private String lastName;
+    private Integer ago;
+    private String phone;
+    private String address;
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAgo() {
+        return ago;
+    }
+
+    public void setAgo(Integer ago) {
+        this.ago = ago;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
